7ac0853dee1ba12d94825f674c3217816f4dd107
Dominic <dominic.let@hotmail.com>
2022-04-06 12:16:00 +0800

    Remove logout button

--- frontend/components/AccountInfo.tsx
+++ frontend/components/AccountInfo.tsx
@@ -11,13 +11,6 @@ const AccountInfo = (props: Props) => {
             <div className="grid grid-cols-3 gap-4">
                 <p className="flex justify-end">{props.user.username}</p>
                 <p className="flex jusitfy-end">{props.user.walletId.slice(0,5) + '...' + props.user.walletId.slice(-3)}</p>
-                    <button 
-                        className="text-black bg-red-300
-                        hover:bg-red-400 focus:ring-4 focus:ring-transparent 
-                        font-medium rounded-full text-base w-full sm:w-auto text-center"
-                        >
-                        Logout
-                    </button>
             </div>
         </div>
     )
