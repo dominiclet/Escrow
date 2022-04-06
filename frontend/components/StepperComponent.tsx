eddbae8a063fafd87715187edc683b61eca1aca1
vivektad <viveksk009@gmail.com>
2022-04-06 10:09:33 +0800

    Change stepper for disputed stage

--- frontend/components/StepperComponent.tsx
+++ frontend/components/StepperComponent.tsx
@@ -6,9 +6,17 @@ interface Props {
 }
 
 const StepperComponent = (props: Props) => {
+    if (props.state == 4) {
         return (
+            <div>
+                <span className="px-2 py-1 font-medium leading-tight text-green-700 bg-green-100 rounded-sm text-center">Disputed</span>
+            </div>
+        )
+    }
+
+    return (
         <div className=''>
-            <Stepper steps={[{ label: 'Awaiting Offer' }, { label: 'Awaiting Acceptance' }, { label: 'Awaiting Performance' }, { label: 'Complete'}, { label: 'Dispute' }]} 
+            <Stepper steps={[{ label: 'Awaiting Offer' }, { label: 'Awaiting Acceptance' }, { label: 'Awaiting Performance' }, { label: 'Complete'}]} 
             activeStep={props.state} />
         </div>
     )
